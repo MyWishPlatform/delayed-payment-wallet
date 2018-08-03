85dfb13b9c91fd316789503a83bb4c5a88f2d6ee
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-08-03 12:49:59 +0300

    use LostKeyDelayedPaymentWallet in lostKetERC20Wallet tests

--- test/lostKeyERC20Wallet.js
+++ test/lostKeyERC20Wallet.js
@@ -9,7 +9,7 @@ const { increaseTime, snapshot, revert } = require('sc-library/test-utils/evmMet
 const { web3async } = require('sc-library/test-utils/web3Utils');
 const getBalance = (address) => web3async(web3.eth, web3.eth.getBalance, address);
 
-const LostKeyERC20Wallet = artifacts.require('./LostKeyERC20Wallet.sol');
+const LostKeyERC20Wallet = artifacts.require('./LostKeyDelayedPaymentWallet.sol');
 const SimpleToken = artifacts.require('./SimpleToken.sol');
 
 const SECOND = 1;
@@ -33,37 +33,37 @@ contract('LostKeyERC20Wallet', function (accounts) {
     });
 
     it('#1 construct', async () => {
-        const contract = await LostKeyERC20Wallet.new(TARGET, [TARGET], [100], 2 * MINUTE);
+        const contract = await LostKeyERC20Wallet.new(TARGET, [TARGET], [100], 2 * MINUTE, 0, 0);
         contract.address.should.have.length(42);
     });
 
     it('#2 successfully sending funds', async () => {
-        const contract = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE);
+        const contract = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE, 0, 0);
         await contract.sendTransaction({ value: web3.toWei(1, 'ether') });
         const balanceBefore = await getBalance(RECIPIENT_2);
 
-        await contract.sendFunds(web3.toWei(0.5, 'ether'), RECIPIENT_2, '', { from: TARGET });
+        await contract.sendFunds(RECIPIENT_2, web3.toWei(0.5, 'ether'), '', { from: TARGET });
         const balanceAfter = await getBalance(RECIPIENT_2);
         balanceAfter.sub(balanceBefore).should.be.bignumber.equal(web3.toWei(0.5, 'ether'));
     });
 
     it('#3 cannot send funds not by target', async () => {
-        const contract = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE);
+        const contract = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE, 0, 0);
         await contract.sendTransaction({ value: web3.toWei(1, 'ether') });
 
-        await contract.sendFunds(web3.toWei(0.5, 'ether'), RECIPIENT_2, '', { from: RECIPIENT_1 })
+        await contract.sendFunds(RECIPIENT_2, web3.toWei(0.5, 'ether'), '', { from: RECIPIENT_1 })
             .should.eventually.be.rejected;
     });
 
     it('#4 token balance', async () => {
-        const wallet = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE);
+        const wallet = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE, 0, 0);
         const token = await SimpleToken.new();
         await token.mint(wallet.address, 100);
         (await wallet.tokenBalanceOf(token.address)).should.be.bignumber.equal(100);
     });
 
     it('#5 token transfer', async () => {
-        const wallet = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE);
+        const wallet = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE, 0, 0);
         const token = await SimpleToken.new();
         await token.mint(wallet.address, 100);
         await wallet.tokenTransfer(token.address, RECIPIENT_1, 100, { from: RECIPIENT_1 })
@@ -73,7 +73,7 @@ contract('LostKeyERC20Wallet', function (accounts) {
     });
 
     it('#6 token approve', async () => {
-        const wallet = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE);
+        const wallet = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE, 0, 0);
         const token = await SimpleToken.new();
         await wallet.tokenApprove(token.address, RECIPIENT_1, 100, { from: RECIPIENT_1 })
             .should.eventually.be.rejected;
@@ -82,7 +82,7 @@ contract('LostKeyERC20Wallet', function (accounts) {
     });
 
     it('#7 token transferFrom', async () => {
-        const wallet = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE);
+        const wallet = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1], [100], MINUTE, 0, 0);
         const token = await SimpleToken.new();
         await token.mint(RECIPIENT_1, 100);
         await token.approve(wallet.address, 100, { from: RECIPIENT_1 });
@@ -91,7 +91,7 @@ contract('LostKeyERC20Wallet', function (accounts) {
     });
 
     it('#8 cannot check before time', async () => {
-        const contract = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1, RECIPIENT_2], [50, 50], 10 * MINUTE);
+        const contract = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1, RECIPIENT_2], [50, 50], 10 * MINUTE, 0, 0);
         await contract.sendTransaction({ value: web3.toWei(1, 'ether') });
 
         const tx = await contract.check({ from: SERVICE_ACCOUNT });
@@ -100,7 +100,7 @@ contract('LostKeyERC20Wallet', function (accounts) {
     });
 
     it('#9 cannot check not by service account', async () => {
-        const contract = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1, RECIPIENT_2], [50, 50], 10 * MINUTE);
+        const contract = await LostKeyERC20Wallet.new(TARGET, [RECIPIENT_1, RECIPIENT_2], [50, 50], 10 * MINUTE, 0, 0);
         await contract.sendTransaction({ value: web3.toWei(1, 'ether') });
         await contract.check({ from: RECIPIENT_1 }).should.eventually.be.rejected;
     });
@@ -111,7 +111,7 @@ contract('LostKeyERC20Wallet', function (accounts) {
         const recipients = [RECIPIENT_1, RECIPIENT_2, RECIPIENT_3];
         const percents = [30, 10, 60];
 
-        const contract = await LostKeyERC20Wallet.new(TARGET, recipients, percents, 10 * MINUTE);
+        const contract = await LostKeyERC20Wallet.new(TARGET, recipients, percents, 10 * MINUTE, 0, 0);
         await contract.sendTransaction({ value: allFunds });
         await increaseTime(15 * MINUTE);
         const balancesBefore = await Promise.all(recipients.map((recipient) => getBalance(recipient)));
@@ -132,7 +132,7 @@ contract('LostKeyERC20Wallet', function (accounts) {
     });
 
     it('#11 cannot set not 100 percents', async () => {
-        await LostKeyERC20Wallet.new(TARGET, [TARGET], [90], MINUTE).should.eventually.be.rejected;
-        await LostKeyERC20Wallet.new(TARGET, [TARGET], [110], MINUTE).should.eventually.be.rejected;
+        await LostKeyERC20Wallet.new(TARGET, [TARGET], [90], MINUTE, 0, 0).should.eventually.be.rejected;
+        await LostKeyERC20Wallet.new(TARGET, [TARGET], [110], MINUTE, 0, 0).should.eventually.be.rejected;
     });
 });
