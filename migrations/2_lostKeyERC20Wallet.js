528accc4d11f0a215f32652223f4fbfa41a7addf
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-08-03 17:33:48 +0300

    add last will tests

--- migrations/2_lostKeyERC20Wallet.js
+++
@@ -1,6 +0,0 @@
-const LostKeyWallet = artifacts.require('./LostKeyERC20Wallet.sol');
-
-module.exports = function (deployer, _, accounts) {
-    const TARGET = accounts[1];
-    deployer.deploy(LostKeyWallet, TARGET, [TARGET], [100], 120);
-};
